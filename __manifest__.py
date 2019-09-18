d7e86a16f27c895591ef8e1ffbf55c4c2f0d8eab
pythonee <dishon.kadoh@gmail.com>
2019-09-18 12:20:46 +0300

    [ADD] Initial Commit

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+# -*- encoding: utf-8 -*-
+
+# Copyright (C) 2019 Dishon Kadoh (<dishon.kadoh@gmail.com>).
+
+{
+    'name': 'Kenya - Accounting',
+    'version': '1.0',
+    'category': 'Localization',
+    'description': """
+        Basic Kenyan Chart of Accounts localisation necessary to run Odoo in Kenya:
+   """,
+    'author': 'Dishon Kadoh',
+    'website': 'dishon.kadoh@gmail.com',
+    'support': 'dishon.kadoh@gmail.com',
+    'license': ' LGPL-3',
+    'depends': ['account', 'base_vat'],
+    'data': [
+        'data/account.account.tag.csv',
+        'data/account.tax.group.csv',
+        'data/account_chart_template_data.xml',
+        'data/account.account.template.csv',
+        'data/account_tax_template_data.xml',
+        'data/account_chart_template_post_data.xml',
+        'data/account_chart_template_configure_data.xml',
+    ],
+}
